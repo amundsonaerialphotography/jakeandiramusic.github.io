fabad0bb4056f96a2ea118ef23dc1b8378292ed6
amundsonaerialphotography <38438753+amundsonaerialphotography@users.noreply.github.com>
2019-05-30 21:43:36 -0600

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,20 @@ function facebookopen() {
 
 }
 
+var password = "block";
+
+if (window.location.search.indexOf('box=allow') > -1) {
+  
+  body.style.display = "none";
+  password = prompt("It's looks too stupid, so If you want to see, enter the password 😏! ");
+  
+  if (password = "iltsm11") {
+    body.style.display = "inline";
+  }
+  
+  
+}else{
+
 alert("Don't laugh; It's getting a redo😎");
 
-
+}
