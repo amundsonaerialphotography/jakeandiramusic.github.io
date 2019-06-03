4937339f26d84a59992fc1daee14aa57fe9272a1
amundsonaerialphotography <38438753+amundsonaerialphotography@users.noreply.github.com>
2019-06-03 14:09:09 -0600

    Update script.js

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ var password = "block";
 if (window.location.href == "http://jakeandiramusic.site/?box=allow") {
   
   alert(window.location.href);
+  body.style.display = "none";
   ppmt("It's looks too stupid, but if you want to see, enter the password! 😏 ");
   
   
